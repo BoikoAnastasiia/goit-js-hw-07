d60962dee6984343472a623c41cd63b8664fe539
anastasyah86 <nastasyah86@gmail.com>
2020-12-16 16:05:39 +0200

    third task copied

---
+++ js/task_7_3.js
@@ -0,0 +1,45 @@
+// Напиши скрипт для создания галлереи изображений по массиву данных.
+
+// В HTML есть список ul#gallery.
+
+// <ul id="gallery"></ul>
+// Используй массив объектов images для создания тегов img вложенных в li.
+// Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
+const images = [
+  {
+    url:
+      "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+
+const createGallery = (pic) => {
+  const item = document.createElement("li");
+  item.classList.add("js-pics");
+  const image = document.createElement("img");
+  image.setAttribute("src", pic.url);
+  image.setAttribute("alt", pic.alt);
+  image.classList.add("js-img");
+  item.append(image);
+  return item;
+};
+
+const imagesList = images.map((slide) => createGallery(slide));
+const galleryRef = document.querySelector("#gallery");
+
+galleryRef.classList.add("js-gallery");
+
+galleryRef.append(...imagesList);

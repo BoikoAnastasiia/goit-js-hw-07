7f2508f932727e3ebfbf85e0ea2cdb0f3b18866a
anastasyah86 <nastasyah86@gmail.com>
2020-12-19 15:51:06 +0200

    4

--- js/task_7_4.js
+++ js/task_7_4.js
@@ -1,35 +1,5 @@
-// Напиши скрипт для создания галлереи изображений по массиву данных.
+// Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение счетчика на 1.
 
-// В HTML есть список ul#gallery.
-
-// <ul id="gallery"></ul>
-// Используй массив объектов images для создания тегов img вложенных в li.
-// Для создания разметки используй шаблонные строки и insertAdjacentHTML().
-
-// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
-// Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
-const images = [
-  {
-    url:
-      "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "White and Black Long Fur Cat",
-  },
-  {
-    url:
-      "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-  },
-  {
-    url:
-      "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "Group of Horses Running",
-  },
-];
-
-const imgListRef = document.querySelector("#gallery");
-
-const imgListString = images.reduce((totalString, element) => {
-  return (totalString += `<li><img src="${element.url}" alt="${element.alt}"/></li>`);
-}, "");
-
-imgListRef.insertAdjacentHTML("afterbegin", imgListString);
+// Создай переменную counterValue в которой будет хранится текущее значение счетчика.
+// Создай функции increment и decrement для увеличения и уменьшения значения счетчика
+// Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
